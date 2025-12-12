7c6d3c47c180aa06e9a810247b470ed369a1eba7
outdead <outdead@mail.ru>
2025-12-12 22:48:16 +0300

    Update release.sh

--- release.sh
+++ release.sh
@@ -69,7 +69,7 @@ function compress_release() {
 }
 
 function install_release() {
-    cp -r  .tmp/release/"${RELEASE_NAME}" ~/Zomboid/Workshop/"${MOD_NAME}"
+    cp -r .tmp/release/"${RELEASE_NAME}" ~/Zomboid/Workshop/"${MOD_NAME}"
     rm -r .tmp/release/"${RELEASE_NAME}"
 }
 
