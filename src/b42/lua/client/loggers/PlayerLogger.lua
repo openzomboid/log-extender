e22673000157c75b18c314dbf8f8a88e10f36a89
outdead <outdead@mail.ru>
2026-08-23 15:52:49 +0300

    Rename loggers

--- src/b42/lua/client/loggers/PlayerLogger.lua
+++ src/b42/lua/client/loggers/PlayerLogger.lua
@@ -6,14 +6,14 @@
 -- Project Zomboid game.
 --
 
-local PlayerClientLogger = {}
+local PlayerLogger = {}
 
-function PlayerClientLogger.IsEnabledOnServer()
+function PlayerLogger.IsEnabledOnServer()
     return SandboxVars.LogExtender.ExtendPlayerLogs
 end
 
 -- DumpPlayer writes player perks and safehouse coordinates to log file.
-function PlayerClientLogger.DumpPlayer(player, action)
+function PlayerLogger.DumpPlayer(player, action)
     if player == nil then
         return nil
     end
@@ -99,39 +99,39 @@ function PlayerClientLogger.DumpPlayer(player, action)
 end
 
 -- OnCreatePlayer adds callback for player OnCreatePlayerData event.
-PlayerClientLogger.OnCreatePlayer = function(id)
-    Events.OnTick.Add(PlayerClientLogger.OnTick)
+PlayerLogger.OnCreatePlayer = function(id)
+    Events.OnTick.Add(PlayerLogger.OnTick)
 end
 
 -- OnPlayerDeath adds callback for player OnPlayerDeath event.
-function PlayerClientLogger.OnPlayerDeath(player)
-	PlayerClientLogger.DumpPlayer(player, "death")
+function PlayerLogger.OnPlayerDeath(player)
+	PlayerLogger.DumpPlayer(player, "death")
 end
 
 -- OnTick creates and removes ticker for emulating player connected event.
 -- This is Black Magic.
-PlayerClientLogger.OnTick = function()
+PlayerLogger.OnTick = function()
     local player = getPlayer()
     if player then
-        Events.OnTick.Remove(PlayerClientLogger.OnTick)
-        PlayerClientLogger.DumpPlayer(player, "connected")
+        Events.OnTick.Remove(PlayerLogger.OnTick)
+        PlayerLogger.DumpPlayer(player, "connected")
     end
 end
 
 -- OnPerkLevel adds callback for player OnPerkLevel global event.
-PlayerClientLogger.OnPerkLevel = function(player, perk, level)
+PlayerLogger.OnPerkLevel = function(player, perk, level)
     if player and perk and level then
         if instanceof(player, 'IsoPlayer') and player:isLocalPlayer() then
             -- Hide events from the log when creating a character.
             if player:getHoursSurvived() <= 0 then return end
 
-            PlayerClientLogger.DumpPlayer(player, "levelup")
+            PlayerLogger.DumpPlayer(player, "levelup")
         end
     end
 end
 
 -- EveryHours adds callback for EveryHours global event.
-PlayerClientLogger.EveryHours = function()
+PlayerLogger.EveryHours = function()
     local player = getSpecificPlayer(0)
     if player and instanceof(player, 'IsoPlayer') and player:isLocalPlayer() then
         -- Hide events from the log when creating a character.
@@ -140,31 +140,31 @@ PlayerClientLogger.EveryHours = function()
         -- Hide events from the log when character is dead.
         if player:isDead() then return end
 
-        PlayerClientLogger.DumpPlayer(player, "tick")
+        PlayerLogger.DumpPlayer(player, "tick")
     end
 end
 
 -- OnGameStart adds callback for OnGameStart global event.
-PlayerClientLogger.OnGameStart = function()
-    local levelup = PlayerClientLogger.IsEnabledOnServer()
-    local tick = PlayerClientLogger.IsEnabledOnServer()
-    local death = PlayerClientLogger.IsEnabledOnServer()
+PlayerLogger.OnGameStart = function()
+    local levelup = PlayerLogger.IsEnabledOnServer()
+    local tick = PlayerLogger.IsEnabledOnServer()
+    local death = PlayerLogger.IsEnabledOnServer()
 
     if levelup then
-        Events.LevelPerk.Add(PlayerClientLogger.OnPerkLevel)
+        Events.LevelPerk.Add(PlayerLogger.OnPerkLevel)
     end
 
     if tick then
-        Events.EveryHours.Add(PlayerClientLogger.EveryHours)
+        Events.EveryHours.Add(PlayerLogger.EveryHours)
     end
 
     if death then
-        Events.OnPlayerDeath.Add(PlayerClientLogger.OnPlayerDeath)
+        Events.OnPlayerDeath.Add(PlayerLogger.OnPlayerDeath)
     end
 end
 
-if PlayerClientLogger.IsEnabledOnServer() then
-    Events.OnCreatePlayer.Add(PlayerClientLogger.OnCreatePlayer)
+if PlayerLogger.IsEnabledOnServer() then
+    Events.OnCreatePlayer.Add(PlayerLogger.OnCreatePlayer)
 end
 
-Events.OnGameStart.Add(PlayerClientLogger.OnGameStart)
+Events.OnGameStart.Add(PlayerLogger.OnGameStart)
--- src/b42/lua/client/loggers/VehicleLogger.lua
+++ src/b42/lua/client/loggers/VehicleLogger.lua
@@ -3,7 +3,7 @@
 -- Use of this source code is governed by the Apache 2.0 license.
 --
 
-local VehicleClientLogger = {
+local VehicleLogger = {
     -- Store vehicle object when user enter to it.
     vehicle = nil,
     -- Store vehicle object when user attach it.
@@ -11,12 +11,12 @@ local VehicleClientLogger = {
     vehicleAttachmentB = nil,
 }
 
-function VehicleClientLogger.IsEnabledOnServer()
+function VehicleLogger.IsEnabledOnServer()
     return SandboxVars.LogExtender.ExtendVehicleLogs
 end
 
 -- DumpVehicle writes vehicles info to log file.
-function VehicleClientLogger.DumpVehicle(player, action, vehicle, vehicle2)
+function VehicleLogger.DumpVehicle(player, action, vehicle, vehicle2)
     if player == nil then
         return nil
     end
@@ -58,27 +58,27 @@ function VehicleClientLogger.DumpVehicle(player, action, vehicle, vehicle2)
 end
 
 -- VehicleEnter adds callback for OnEnterVehicle event.
-VehicleClientLogger.VehicleEnter = function(player)
+VehicleLogger.VehicleEnter = function(player)
     if player and instanceof(player, 'IsoPlayer') and player:isLocalPlayer() then
-        if VehicleClientLogger.vehicle == nil then
-            VehicleClientLogger.vehicle = player:getVehicle()
-            VehicleClientLogger.DumpVehicle(player, "enter", VehicleClientLogger.vehicle, nil)
+        if VehicleLogger.vehicle == nil then
+            VehicleLogger.vehicle = player:getVehicle()
+            VehicleLogger.DumpVehicle(player, "enter", VehicleLogger.vehicle, nil)
         end
     end
 end
 
 -- VehicleExit adds callback for OnExitVehicle event.
-VehicleClientLogger.VehicleExit = function(player)
+VehicleLogger.VehicleExit = function(player)
     if player and instanceof(player, 'IsoPlayer') and player:isLocalPlayer() then
-        if VehicleClientLogger.vehicle ~= nil then
-            VehicleClientLogger.DumpVehicle(player, "exit", VehicleClientLogger.vehicle, nil)
-            VehicleClientLogger.vehicle = nil
+        if VehicleLogger.vehicle ~= nil then
+            VehicleLogger.DumpVehicle(player, "exit", VehicleLogger.vehicle, nil)
+            VehicleLogger.vehicle = nil
         end
     end
 end
 
 -- VehicleAttach adds callback for ISAttachTrailerToVehicle event.
-VehicleClientLogger.VehicleAttach = function()
+VehicleLogger.VehicleAttach = function()
     local originalPerform = ISAttachTrailerToVehicle.perform
 
     ISAttachTrailerToVehicle.perform = function(self)
@@ -87,21 +87,21 @@ VehicleClientLogger.VehicleAttach = function()
         local player = self.character
 
         if player then
-            VehicleClientLogger.vehicleAttachmentA = self.vehicleA
-            VehicleClientLogger.vehicleAttachmentB = self.vehicleB
-            VehicleClientLogger.DumpVehicle(player, "attach", self.vehicleA, self.vehicleB)
+            VehicleLogger.vehicleAttachmentA = self.vehicleA
+            VehicleLogger.vehicleAttachmentB = self.vehicleB
+            VehicleLogger.DumpVehicle(player, "attach", self.vehicleA, self.vehicleB)
         end
     end
 end
 
 -- VehicleDetach adds callback for ISDetachTrailerFromVehicle event.
-VehicleClientLogger.VehicleDetach = function()
+VehicleLogger.VehicleDetach = function()
     local originalPerform = ISDetachTrailerFromVehicle.perform
 
     ISDetachTrailerFromVehicle.perform = function(self)
         local vehicleB = self.vehicle:getVehicleTowing()
         if vehicleB == nil then
-            vehicleB = VehicleClientLogger.vehicleAttachmentB
+            vehicleB = VehicleLogger.vehicleAttachmentB
         end
 
         originalPerform(self)
@@ -109,9 +109,9 @@ VehicleClientLogger.VehicleDetach = function()
         local player = self.character
 
         if player then
-            VehicleClientLogger.DumpVehicle(player, "detach", self.vehicle, vehicleB)
-            VehicleClientLogger.vehicleAttachmentA = nil
-            VehicleClientLogger.vehicleAttachmentB = nil
+            VehicleLogger.DumpVehicle(player, "detach", self.vehicle, vehicleB)
+            VehicleLogger.vehicleAttachmentA = nil
+            VehicleLogger.vehicleAttachmentB = nil
         end
     end
 end
@@ -122,7 +122,7 @@ end
 
 -- ISSpawnVehicleUI_onClick adds logs record to admin.txt file after spawn, repair
 -- vehicle and add key from vehicle in Spawn Vehicle interface.
-VehicleClientLogger.ISSpawnVehicleUI_onClick = function()
+VehicleLogger.ISSpawnVehicleUI_onClick = function()
     local originalOnClick = ISSpawnVehicleUI.onClick
 
     ISSpawnVehicleUI.onClick = function(self, button)
@@ -162,7 +162,7 @@ VehicleClientLogger.ISSpawnVehicleUI_onClick = function()
     end
 end
 
-VehicleClientLogger.DebugContextMenuCheats = function()
+VehicleLogger.DebugContextMenuCheats = function()
     local originalOnAddVehicle = DebugContextMenu.onAddVehicle
 
     DebugContextMenu.onAddVehicle = function(playerObj)
@@ -194,7 +194,7 @@ end
 
 -- OnAddVehicleCommand adds logs record to admin.txt file after spawn vehicle
 -- from chat command.
-VehicleClientLogger.OnAddVehicleCommand = function()
+VehicleLogger.OnAddVehicleCommand = function()
     local onCommandEnteredOriginal = ISChat.onCommandEntered
 
     ISChat.onCommandEntered = function(self)
@@ -243,7 +243,7 @@ VehicleClientLogger.OnAddVehicleCommand = function()
     end
 end
 
-VehicleClientLogger.OnCheatRemove = function()
+VehicleLogger.OnCheatRemove = function()
     local onCheatRemoveAuxOriginal = ISVehicleMechanics.onCheatRemoveAux
 
     ISVehicleMechanics.onCheatRemoveAux = function(dummy, button, playerObj, vehicle)
@@ -261,7 +261,7 @@ VehicleClientLogger.OnCheatRemove = function()
     end
 end
 
-VehicleClientLogger.OnCheatRepair = function()
+VehicleLogger.OnCheatRepair = function()
     local onCheatRepairOriginal = ISVehicleMechanics.onCheatRepair
 
     ISVehicleMechanics.onCheatRepair = function(playerObj, vehicle)
@@ -277,7 +277,7 @@ VehicleClientLogger.OnCheatRepair = function()
     end
 end
 
-VehicleClientLogger.OnCheatRepairPart = function()
+VehicleLogger.OnCheatRepairPart = function()
     local onCheatRepairPartOriginal = ISVehicleMechanics.onCheatRepairPart
 
     ISVehicleMechanics.onCheatRepairPart = function(playerObj, part)
@@ -293,7 +293,7 @@ VehicleClientLogger.OnCheatRepairPart = function()
     end
 end
 
-VehicleClientLogger.OnCheatSetCondition = function()
+VehicleLogger.OnCheatSetCondition = function()
     local onCheatSetConditionAuxOriginal = ISVehicleMechanics.onCheatSetConditionAux
 
     ISVehicleMechanics.onCheatSetConditionAux = function(target, button, playerObj, part)
@@ -309,7 +309,7 @@ VehicleClientLogger.OnCheatSetCondition = function()
     end
 end
 
-VehicleClientLogger.OnCheatGetKey = function()
+VehicleLogger.OnCheatGetKey = function()
     local onCheatGetKeyOriginal = ISVehicleMechanics.onCheatGetKey
 
     ISVehicleMechanics.onCheatGetKey = function(playerObj, vehicle)
@@ -325,29 +325,29 @@ VehicleClientLogger.OnCheatGetKey = function()
     end
 end
 
-if VehicleClientLogger.IsEnabledOnServer() then
-    Events.OnEnterVehicle.Add(VehicleClientLogger.VehicleEnter)
+if VehicleLogger.IsEnabledOnServer() then
+    Events.OnEnterVehicle.Add(VehicleLogger.VehicleEnter)
 end
 
-if VehicleClientLogger.IsEnabledOnServer() then
-    Events.OnExitVehicle.Add(VehicleClientLogger.VehicleExit)
+if VehicleLogger.IsEnabledOnServer() then
+    Events.OnExitVehicle.Add(VehicleLogger.VehicleExit)
 end
 
-if VehicleClientLogger.IsEnabledOnServer() then
-    VehicleClientLogger.VehicleAttach()
+if VehicleLogger.IsEnabledOnServer() then
+    VehicleLogger.VehicleAttach()
 end
 
-if VehicleClientLogger.IsEnabledOnServer() then
-    VehicleClientLogger.VehicleDetach()
+if VehicleLogger.IsEnabledOnServer() then
+    VehicleLogger.VehicleDetach()
 end
 
 if SandboxVars.LogExtender.VehicleAdminTools then
-    VehicleClientLogger.ISSpawnVehicleUI_onClick()
-    VehicleClientLogger.DebugContextMenuCheats()
-    VehicleClientLogger.OnAddVehicleCommand()
-    VehicleClientLogger.OnCheatRemove()
-    VehicleClientLogger.OnCheatRepair()
-    VehicleClientLogger.OnCheatRepairPart()
-    VehicleClientLogger.OnCheatSetCondition()
-    VehicleClientLogger.OnCheatGetKey()
+    VehicleLogger.ISSpawnVehicleUI_onClick()
+    VehicleLogger.DebugContextMenuCheats()
+    VehicleLogger.OnAddVehicleCommand()
+    VehicleLogger.OnCheatRemove()
+    VehicleLogger.OnCheatRepair()
+    VehicleLogger.OnCheatRepairPart()
+    VehicleLogger.OnCheatSetCondition()
+    VehicleLogger.OnCheatGetKey()
 end
